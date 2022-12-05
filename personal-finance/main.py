f6f3d8cd87b675a3e5bc8ccf5e5cb7d72e013233
Felipe Borba <felipe.borba0109@gmail.com>
2022-12-04 22:24:52 -0300

    personal-finance: add more function in user inputs

--- personal-finance/main.py
+++ personal-finance/main.py
@@ -1,69 +1,78 @@
 import datetime
-from datetime import date
 
 from expenses import Expenses
 
 
-def main():
-    expense = Expenses()
-    while True:
-        print('\n---\nDeseja cadastrar um ganho ou gasto?\nver extrato?\ndigite 0 para sair')
-        command = input()
+def prompt_date():
+    try:
+        user_input = input('Digite uma Data [dd-mm-yyyy]')
+        return datetime.datetime.strptime(user_input, '%d-%m-%Y').date()
+    except Exception:
+        raise ValueError('Data inválida')
 
+
+def prompt_user_expense():
+    try:
+        raw_value = input('Digite um valor')
+        value = float(raw_value)
+    except Exception:
+        raise ValueError('Valor inválido')
+
+    try:
+        raw_kind = input('Digite um tipo')
+        kind = raw_kind.strip()
+    except Exception:
+        raise ValueError('Tipo inválido')
+
+    try:
+        raw_note = input('Digite uma nota')
+        note = raw_note.strip()
+    except Exception:
+        raise ValueError('Nota inválida')
+
+    return value, kind, note
+
+
+def handle_user_input(expenses: Expenses):
+    while True:
         try:
-            match command:
+            print('\n-------------------------\n')
+            print('[X] Digite uma opção abaixo')
+            print('[0] Sair')
+            print('[1] Cadastrar ganho')
+            print('[2] Cadastrar gasto')
+            print('[3] Ver saldo por mês')
+            print('[4] Ver extrato')
+            print('[5] Var valor total em conta')
+            print('[6] Ver gastos por tipo')
+
+            match input():
                 case '0':
                     print('bye')
                     return
-                case 'ganho':
-                    day, montante, month, obs, tipo, year = get_input_value()
+                case '1':
+                    [value, kind, note] = prompt_user_expense()
+                    user_date = prompt_date()
 
-                    validate_date(year, month, day)
-                    input_date = date(year=int(year), month=int(month), day=int(day))
+                    expense.add_income(user_date, value, kind, note)
+                case '2':
+                    [value, kind, note] = prompt_user_expense()
+                    user_date = prompt_date()
 
-                    expense.add_income(input_date, montante, tipo, obs)
-                case 'gasto':
-                    day, montante, month, obs, tipo, year = get_input_value()
-
-                    validate_date(year, month, day)
-                    input_date = date(year=int(year), month=int(month), day=int(day))
-
-                    expense.add_spending(input_date, montante, tipo, obs)
-                case 'extrato':
-                    print(expense)
+                    expense.add_spending(user_date, value, kind, note)
+                case '3':
+                    balance = expenses.get_balance_by_month()
+                    print(balance)
+                case '4':
+                    print(expenses)
+                case '5':
+                    print('Valor total em conta:\n', expenses.get_balance())
+                case '6':
+                    print('Total de gastos por Tipo:\n', expenses.get_expenses_by_kind())
                 case _:
-                    print('Entrada invalida')
-                    pass
-
-            print('Valor total em conta:', expense.get_balance())
-            print('Total de gastos por Tipo:')
-            print_gastos_por_tipo(expense)
-
-        except:
-            pass
-
-
-def get_input_value():
-    try:
-        values = input('[ano, mes, dia, valor, tipo, obs]\n')
-        [ano, mes, dia, valor, tipo, obs] = values.split(',')
-        year = str(ano).strip()
-        month = str(mes).strip()
-        day = str(dia).strip()
-        montante = float(valor)
-        return day, montante, month, obs, tipo, year
-    except:
-        print('formato da entrada errada\n')
-        raise
-
-
-def validate_date(year, month, day):
-    try:
-        date_str = year + '-' + month + '-' + day
-        datetime.datetime.strptime(date_str, '%Y-%m-%d')
-    except:
-        print("Data invalida")
-        raise
+                    print('Opção Inválida')
+        except Exception as error:
+            print(error)
 
 
 def print_gastos_por_tipo(gastos):
@@ -73,4 +82,5 @@ def print_gastos_por_tipo(gastos):
 
 
 if __name__ == '__main__':
-    main()
+    expense = Expenses()
+    handle_user_input(expense)
